dd279d4dacfced7b4948e9a2df7ad910b82a99a9
tristan <mytic12@hotmail.fr>
2022-06-03 15:50:54 +0200

    Update TopBanner, HomePage

--- components/TopBanner.jsx
+++ components/TopBanner.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const TopBanner = () => {
+import { urlFor } from '../lib/client';
+
+const Topbanner = ({ topBanner }) => {
   return (
-    <div>TopBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{topBanner.smallText}</p>
+        <h3>{topBanner.midText}</h3>
+    
+            <h5>Description</h5>
+            <p>{topBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default TopBanner
+export default Topbanner
