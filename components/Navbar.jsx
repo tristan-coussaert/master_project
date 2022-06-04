7e1b872fcd6f2740c4ce56d7066cba3b9d4fdd52
tristan-coussaert <mytic12@hotmail.fr>
2022-06-04 19:47:06 +0200

    Add Navbar :sparkles:

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import { Cart } from './';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">AmazonLike</Link>
+      </p>
+
+      <button type="button" className="cart-icon">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
   )
 }
 
