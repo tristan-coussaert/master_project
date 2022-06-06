643d7b86ac7b580fee503a5e923f4ba6dda67df1
tristan-coussaert <mytic12@hotmail.fr>
2022-06-07 00:23:30 +0200

    Add Header :sparkles:

---
+++ front/src/components/headers/Header.js
@@ -0,0 +1,86 @@
+import React, {useContext, useState} from 'react'
+import {GlobalState} from '../../GlobalState'
+import Menu from './icon/menu.svg'
+import Close from './icon/close.svg'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+import { AiOutlineShopping } from 'react-icons/ai'
+
+function Header() {
+    const state = useContext(GlobalState)
+    const [isLogged] = state.userAPI.isLogged
+    const [isAdmin] = state.userAPI.isAdmin
+    const [cart] = state.userAPI.cart
+    const [menu, setMenu] = useState(false)
+
+    const logoutUser = async () =>{
+        await axios.get('/user/logout')
+        
+        localStorage.removeItem('firstLogin')
+        
+        window.location.href = "/";
+    }
+
+    const adminRouter = () =>{
+        return(
+            <>
+                <li><Link to="/create_product">Créer un produit</Link></li>
+                <li><Link to="/category">Catégories</Link></li>
+            </>
+        )
+    }
+
+    const loggedRouter = () =>{
+        return(
+            <>
+                <li><Link to="/" onClick={logoutUser}>Déconnexion</Link></li>
+            </>
+        )
+    }
+
+
+    const styleMenu = {
+        left: menu ? 0 : "-100%"
+    }
+
+    return (
+        <header>
+            <div className="menu" onClick={() => setMenu(!menu)}>
+                <img src={Menu} alt="" width="30" />
+            </div>
+
+            <div className="logo">
+                <h1>
+                    <Link to="/">{isAdmin ? 'Admin' : 'Amazon Like'}</Link>
+                </h1>
+            </div>
+
+            <ul style={styleMenu}>
+                
+                {isAdmin && adminRouter()}
+
+                {
+                    isLogged ? loggedRouter() : <li><Link to="/login">Connexion ✥ Créer un compte</Link></li>
+                }
+
+                <li onClick={() => setMenu(!menu)}>
+                    <img src={Close} alt="" width="30" className="menu" />
+                </li>
+
+            </ul>
+
+            {
+                isAdmin ? '' 
+                :<div className="cart-icon">
+                    <span>{cart.length}</span>
+                    <Link to="/cart">
+                    <AiOutlineShopping />
+                    </Link>
+                </div>
+            }
+            
+        </header>
+    )
+}
+
+export default Header
