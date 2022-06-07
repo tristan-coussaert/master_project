af12232d966a215ec01284607e9334a84d127cca
tristan-coussaert <mytic12@hotmail.fr>
2022-06-07 12:18:42 +0200

    modif ProductItem

--- front/src/components/mainpages/utils/productItem/ProductItem.js
+++ front/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import BtnRender from './BtnRender';
 
-function ProductItem({ product, isAdmin, handleCheck }) {
+function ProductItem({ product, isAdmin, deleteProduct, handleCheck }) {
   return (
     <div className="product_card">
-      <Link to={`/detail/${product._id}`}>
-        {isAdmin && (
-          <input
-            type="checkbox"
-            checked={product.checked}
-            onChange={() => handleCheck(product._id)}
-          />
-        )}
-        <img src={product.images.url} alt="" />
+            {
+                isAdmin && <input type="checkbox" checked={product.checked}
+                onChange={() => handleCheck(product._id)} />
+            }
+            <img src={product.images.url} alt="" />
 
-        <div className="product_box">
-          <h2 title={product.title}>{product.title}</h2>
-          <span>{product.price} €</span>
+            <div className="product_box">
+                <h2 title={product.title}>{product.title}</h2>
+                <span>${product.price}</span>
+                <p>{product.description}</p>
+            </div>
+
+            
+            <BtnRender product={product} deleteProduct={deleteProduct} />
         </div>
-      </Link>
-    </div>
   );
 }
 
